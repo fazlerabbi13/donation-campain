c7c9276b091487944b2a999abd474f56d0da4efb
fazlerabbi13 <mohammad0132794@gmail.com>
2023-09-28 21:44:50 +0600

    if you click on a card ,it show for donation.if you click donate button one time it show sweet alart.and if you click more then one time that shows a error alart.this features added

--- src/Pages/Card/DonatonCard.jsx
+++ src/Pages/Card/DonatonCard.jsx
@@ -1,8 +1,36 @@
 
+import swal from 'sweetalert';
 import './Overlay.css';
 import { BiDollar } from "react-icons/bi";
 const DonatonCard = ({card}) => {
     const {id,img,category,title,description2,category_bg_color,card_bg_color,text_color,button_bg_color,price} = card 
+    
+    const handleDonationAdd = () =>{
+        const addDonation = [];
+
+        const donationCard =JSON.parse(localStorage.getItem('donation'))
+
+        if(!donationCard){
+            addDonation.push(card)
+            localStorage.setItem('donation',JSON.stringify(addDonation))
+            swal("Good job!", "Donation successful!", "success");
+        }
+        else{
+
+            const isExist =donationCard.find(card =>card.id == id)
+            if(!isExist){
+                addDonation.push(...donationCard,card)
+            localStorage.setItem('donation',JSON.stringify(addDonation))
+            swal("Good job!", "You clicked the button!", "success");
+
+            }
+            else{
+                swal("Error!", "Already donate!", "error");
+            }
+           
+        }
+    }
+    
     return (
         <div>
             <div className="card w-[600px] text-center mx-auto mt-20 bg-base-100 shadow-xl">
